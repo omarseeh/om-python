9039eff4a522317181e50767fa2fe054b90c24ac
omar seeh <omarseeh@icloud.com>
2020-09-12 09:21:02 +0300

    FIRST COMMIT

---
+++ strings.py
@@ -0,0 +1,27 @@
+name='omar'
+print (id(name))
+
+
+age=22 
+value=220.2222
+value=True
+
+fname="omar"
+
+
+lname="SERRIEH"
+
+full_name="OMAR SERRIEH"
+
+print (full_name.replace("OMAR","ALI"))
+B = "n" * 500
+print (B)
+
+
+
+
+
+
+
+
+

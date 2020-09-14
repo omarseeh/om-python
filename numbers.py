e1e1f461b13dc8ba725dc486902cc4be1379ceee
omar seeh <omarseeh@icloud.com>
2020-09-14 13:01:13 +0300

    add numbers

---
+++ numbers.py
@@ -0,0 +1,20 @@
+print(2+3)
+print(6-4)
+print(5*5)
+print(8/2)
+print(3**10)
+print("20"  +"" +   "40")
+print(33/2)
+
+
+
+num1 = 21
+num2 = 66
+num3 = num1 / num2
+
+
+print(num3)
+
+print (type(num3))
+num2 +=2
+print(num2)

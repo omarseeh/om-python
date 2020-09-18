1ced0e5ac1fe5c8ca607e1a54cc262ac123d009c
omar seeh <omarseeh@icloud.com>
2020-09-18 19:57:58 +0300

    third commit

--- logic operations.py
+++ logic operations.py
@@ -1,24 +1,40 @@
-num1 =4
-num2 =9
-print(num1 < num2)
-print(num1 > num2)
-print(5 < 8)
-print(5 > 8)
-print(5 == 8)
-print(6 != 8)
-print(6 >= 8)
-print (6 <= 8)
+# num1 =4
+# num2 =9
+# print(num1 < num2)
+# print(num1 > num2)
+# print(5 < 8)
+# print(5 > 8)
+# print(5 == 8)
+# print(6 != 8)
+# print(6 >= 8)
+# print (6 <= 8)
 
-print( True and True ) # True
-print (True and False ) # False
-print (False and False) # False
+# print( True and True ) # True
+# print (True and False ) # False
+# print (False and False) # False
+a,b=1,3
+print(a, b)
+# print(a==a and b==b) #true
+# print(a==a and a>b) #false
+# print(a<b and b<a) #false
 
-print(True or False)#true
-print(True or True)#true
-print(False or False )#false
+print (a==a or b==b) #true
+print (a<b or b==b) #true
+print (a>b or b==b) #true
+print (a>b or b<a) #false
 
-print(not True) #FALSE
-print(not False) #TRUE
+
+
+
+
+
+
+# print(True or False)#true
+# print(True or True)#true
+# print(False or False )#false
+
+# print(not True) #FALSE
+# print(not False) #TRUE
 
 
 

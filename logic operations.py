7df123b559bf1034d9a42dd4ba4a55d0e103066d
omar seeh <omarseeh@icloud.com>
2020-09-18 11:23:02 +0300

    SECOND COMMIT

--- logic operations.py
+++ logic operations.py
@@ -13,5 +13,12 @@ print( True and True ) # True
 print (True and False ) # False
 print (False and False) # False
 
+print(True or False)#true
+print(True or True)#true
+print(False or False )#false
+
+print(not True) #FALSE
+print(not False) #TRUE
+
 
 

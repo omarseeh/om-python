2d2fa439e062c311ab97c63c069062b4f163df8f
omar seeh <omarseeh@icloud.com>
2020-09-21 12:43:12 +0300

    commit 1

---
+++ conditions.py
@@ -0,0 +1,7 @@
+username =input("please enter your username: ")
+password =input("please enter your password: ")
+if username == "omar" and password=="123" :
+    print("wellcome to your email") 
+else:
+         print("error")
+

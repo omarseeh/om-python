77a576e9032a2ec7eb957599a343003ddcee9135
omar seeh <omarseeh@icloud.com>
2020-09-23 20:45:12 +0300

    first commit

---
+++ while loop.py
@@ -0,0 +1,9 @@
+num_of_tries=0
+username = input("please insert your username: ")
+password = input("please insert your password: ")
+if username== "omar" and password == "123":
+ print ("wellcome")
+else : 
+ print ("error")
+
+

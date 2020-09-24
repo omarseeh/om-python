a1049aecc1e81782d65017c273550e99033785c0
omar seeh <omarseeh@icloud.com>
2020-09-24 17:59:38 +0300

    second commit

--- while loop.py
+++ while loop.py
@@ -1,9 +1,20 @@
 num_of_tries=0
-username = input("please insert your username: ")
-password = input("please insert your password: ")
-if username== "omar" and password == "123":
- print ("wellcome")
-else : 
- print ("error")
+while num_of_tries !=3:
+  username = input("please insert your username: ")
+  password = input("please insert your password: ")
+
+  if username == "omar" and password == "123":
+        print ("wellcome")
+        num_of_tries=0
+        exit 
+
+
+  else : 
+        print ("error")
+        num_of_tries +=1
+        print (" you have " + str(3 - num_of_tries)+ " tries left ")
+        
+
+
 
 

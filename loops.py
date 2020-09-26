8efa58b7d98c786d6e8173936450221c4629ae4b
omar seeh <omarseeh@icloud.com>
2020-09-26 15:37:20 +0300

    first coomit

---
+++ loops.py
@@ -0,0 +1,26 @@
+subjects =["english" , "maths" ,"arabic"]
+
+# print(subjects)
+# # print(subjects [0])
+# # print(subjects[1])
+
+subjects.append("history")
+# # # print(subjects[0:3])
+# # # print(subjects[1:3])
+# # # print(subjects[1:4])
+# # # print(subjects[1:6])
+# # # print(subjects[-1])
+
+subjects.pop(1)
+# # print(subjects)
+
+# subjects.pop()
+# print(subjects)
+
+
+for x in subjects:
+  print(x)
+    
+
+
+

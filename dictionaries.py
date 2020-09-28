3d376197e68b93a251cdda5d0385dbdb800457fe
omar seeh <omarseeh@icloud.com>
2020-09-28 14:33:39 +0300

    fistr dictionary

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+# subjects_with_no_students={"arabic":3,"maths":3,"english":2}
+# print(subjects_with_no_students)
+students={"arabic":("omar","ali","aber"),"english":("jhon","sam","sw"),"maths":("salem","hussian","ahmad")}
+print(students)
+students["maths"]=["salah","nadia"]
+print(students)
+students.update({"history":"alas"})
+print(students)
+
+

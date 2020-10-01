7a13e206e22ed2c2ef001f732327f9c67f5c1ffd
omar seeh <omarseeh@icloud.com>
2020-10-01 16:27:52 +0300

    comy first

---
+++ functions.py
@@ -0,0 +1,24 @@
+# def linear():
+#     print(".........................")
+# print("omar salem")
+# linear()
+# print("22")
+# linear()
+
+def addition(num1,num2):
+    print(int(num1) + int(num2))
+def subtract(num1,num2):
+    print(int(num1) - int(num2))
+def multiply(num1, num2):
+    print(int(num1) * int(num2))
+def division(num1,num2):
+    print(int(num1) / int(num2))
+
+num1=input("please enter the first number: ")
+num2=input("please enter the second number: ")
+addition(num1,num2)
+subtract(num1,num2)
+multiply(num1,num2)
+division(num1,num2)
+
+
